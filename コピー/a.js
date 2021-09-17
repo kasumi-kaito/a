cd38777935448c402a349a569cd3e88f7e06a894
kasumi-kaito <79437914+kasumi-kaito@users.noreply.github.com>
2021-09-17 11:56:17 +0900

    Add files via upload

---
+++ コピー/a.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+      r();
+      load();
+}
+function r(){
+      i = 3600; }
+var a = function(){
+          i --;
+      u = document.getElementById('p')   
+u.innerHTML=　'残り'　+ i + '秒';
+     if(i < 600){
+     u.className = 't'
+     }
+     if(i==0){ 
+           clearInterval(g);
+      }
+            localStorage.setItem('残り時間',i)   
+}
+ g = setInterval(a,1000)
+      function load(){
+            i = localStorage.getItem('残り時間',i)
+            }
+            function z(){
+                  clearInterval(g)
+            }
+     　function y(){
+            i = 3600;
+            localStorage.setItem('残り時間',i)    
+      }
